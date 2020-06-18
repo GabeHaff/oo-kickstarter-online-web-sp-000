b8edb0db2c31cc7c373e778d08e9aafb88d91c41
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-06-18 01:55:51 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,20 +1,15 @@
 class Project
-  attr_reader :backers, :title
+  attr_reader :backers
   
   def initialize(title)
    @title = title 
    @backers = []
   end 
 
-#init w/ @backers =[]; attr_reader :backers
-
-#takes in Backer instance, @backers << backer
 
 def add_backer(backer=Backer)
   @backers << backer
   backer.backed_projects << self 
- # backer.add_project(self)
-  #     project.add_backer(self)
-
+ 
 end 
 end #class end 
