facfae26f44b60c1156521b287424b11735a91b7
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-06-18 01:59:25 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,7 +10,5 @@ class Project
 def add_backer(backer=Backer)
   @backers << backer
   backer.backed_projects << self 
- #adds this instance of project to the Backer class' @backed_projects array, 
- #much like project.backers << self (#<backer>)
 end 
 end #class end 
